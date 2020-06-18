4a6545c2327e250649882e800f84a5c83d49733d
Chris Howarth <chowarth23@gmail.com>
2020-06-18 19:11:04 +0100

    Update profile endpoint to get more detailed presence information

--- src/MyPlayStation.API/Interfaces/IUserProfileApi.cs
+++ src/MyPlayStation.API/Interfaces/IUserProfileApi.cs
@@ -8,7 +8,7 @@ namespace MyPlayStation.API.Interfaces
     [Headers( "Authorization: Bearer" )]
     public interface IUserProfileApi
     {
-        [Get( "/userProfile/v1/users/{request.OnlineId}/profile2?fields=onlineId%2CaboutMe%2CconsoleAvailability%2ClanguagesUsed%2CavatarUrls%2CpersonalDetail%2CpersonalDetail(%40default%2CprofilePictureUrls)%2CprimaryOnlineStatus%2CtrophySummary(level%2Cprogress%2CearnedTrophies)%2Cplus%2CisOfficiallyVerified%2CfriendRelation%2CpersonalDetailSharing%2Cpresences(%40default%2Cplatform)%2CnpId%2Cblocking%2Cfollowing%2CcurrentOnlineId%2CdisplayableOldOnlineId%2CmutualFriendsCount%2CfollowerCount&profilePictureSizes=s%2Cm%2Cl&avatarSizes=s%2Cm%2Cl&languagesUsedLanguageSet=set4" )]
+        [Get( "/userProfile/v1/users/{request.OnlineId}/profile2?fields=onlineId%2CaboutMe%2CconsoleAvailability%2ClanguagesUsed%2CavatarUrls%2CpersonalDetail%2CpersonalDetail(%40default%2CprofilePictureUrls)%2CprimaryOnlineStatus%2CtrophySummary(level%2Cprogress%2CearnedTrophies)%2Cplus%2CisOfficiallyVerified%2CfriendRelation%2CpersonalDetailSharing%2Cpresences(%40default%2C%40titleInfo%2ClastOnlineDate%2Cplatform)%2CnpId%2Cblocking%2Cfollowing%2CcurrentOnlineId%2CdisplayableOldOnlineId%2CmutualFriendsCount%2CfollowerCount&profilePictureSizes=s%2Cm%2Cl&avatarSizes=s%2Cm%2Cl&languagesUsedLanguageSet=set4" )]
         Task<GetProfileResponse> GetProfileAsync( GetProfileRequest request );
     }
 }
