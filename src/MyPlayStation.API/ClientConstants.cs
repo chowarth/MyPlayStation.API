db73105073417b92f49be801da357e1439164158
Chris Howarth <chowarth23@gmail.com>
2020-06-19 15:20:46 +0100

    Add login url and npsso script to ClientConstants

--- src/MyPlayStation.API/ClientConstants.cs
+++ src/MyPlayStation.API/ClientConstants.cs
@@ -3,6 +3,34 @@ namespace MyPlayStation.API
 {
     public class ClientConstants
     {
+        /// <summary>
+        /// Url to the Sony PSN login web page
+        /// </summary>
+        public const string LoginUrl = "https://account.sonyentertainmentnetwork.com/";
+
+        /// <summary>
+        /// The JavaScript that is injected into the Sony PSN login web page to get the NPSSO token
+        /// that can then be used to request authentication tokens.
+        /// </summary>
+        public const string LoginJavaScript =
+            "(function(open) {" +
+                "XMLHttpRequest.prototype.open = function( method, url, async, user, pass)" +
+                "{" +
+                    "this.addEventListener( 'readystatechange', function() {" +
+                        "if( this.readyState == XMLHttpRequest.DONE )" +
+                        "{" +
+                            "let response = JSON.parse( this.responseText );" +
+                            "if( response && 'npsso' in response) {" +
+                                "console.log( 'Found npsso:', response.npsso );" +
+                                "invokeCallback( response.npsso )" +
+                            "}" +
+                        "}" +
+                    "}, false);" +
+
+                    "open.call( this, method, url, async, user, pass );" +
+                "};" +
+            "})(XMLHttpRequest.prototype.open);";
+
         public const string AppContext = "inapp_ios";
         public const string Id = "ebee17ac-99fd-487c-9b1e-18ef50c39ab5";
         public const string Duid = "0000000d00040080027BC1C3FBB84112BFC9A4300A78E96A";
