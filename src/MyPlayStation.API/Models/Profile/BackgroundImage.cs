da8c5f7ed679567a2c3094e520be2c93396cba6d
Chris Howarth <chowarth23@gmail.com>
2020-08-01 10:46:42 +0100

    Move BackgroundImage to the Models folder

---
+++ src/MyPlayStation.API/Models/Profile/BackgroundImage.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace MyPlayStation.API.Models.Profile
+{
+    public class BackgroundImage
+    {
+        [JsonProperty( "sourceUrl" )]
+        public string SourceUrl { get; set; }
+
+        [JsonProperty( "resizeTemplate" )]
+        public string ResizeTemplate { get; set; }
+
+        [JsonProperty( "color" )]
+        public string Colour { get; set; }
+    }
+}
--- src/MyPlayStation.API/Responses/GetProfileBackgroundImageResponse.cs
+++ src/MyPlayStation.API/Responses/GetProfileBackgroundImageResponse.cs
@@ -1,19 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using MyPlayStation.API.Models.Profile;
+using Newtonsoft.Json;
 
 namespace MyPlayStation.API.Responses
 {
-    public class BackgroundImage
-    {
-        [JsonProperty( "sourceUrl" )]
-        public string SourceUrl { get; set; }
-
-        [JsonProperty( "resizeTemplate" )]
-        public string ResizeTemplate { get; set; }
-
-        [JsonProperty( "color" )]
-        public string Colour { get; set; }
-    }
-
     public class GetProfileBackgroundImageResponse
     {
         [JsonProperty( "backgroundImage" )]
