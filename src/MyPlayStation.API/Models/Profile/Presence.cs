52e3f883e4e711ca4d7151aa91e1d4d2dd025fa4
Chris Howarth <chowarth23@gmail.com>
2021-05-20 20:41:15 +0100

    Add platform to presence

--- src/MyPlayStation.API/Models/Profile/Presence.cs
+++ src/MyPlayStation.API/Models/Profile/Presence.cs
@@ -1,7 +1,10 @@
-﻿
+﻿using Newtonsoft.Json;
+
 namespace MyPlayStation.API.Models.Profile
 {
     public class Presence
     {
+        [JsonProperty( "platform" )]
+        public string Platform { get; set; }
     }
 }
